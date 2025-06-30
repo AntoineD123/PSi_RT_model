f245e034fcaea7a669d23f96a5d58d62ee3d8d06
AntoineD123 <antoinedlvp@gmail.com>
2025-06-30 14:29:58 +0200

    Reducede version of tmm, improvements required for efficiency

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@
 import numpy as np
 from n_index import get_n_eff
 # Using https://doi.org/10.5281/zenodo.1344878
-from reflectance import tmm_get_R_T, save_data, load_prev_data, plot_show_R_T, plot_compare_R
+from reflectance import allocate_matrices, tmm_get_R_T, save_data, load_prev_data, plot_show_R_T, plot_compare_R
 from set_layers import compute_t, get_opt_porosities
 from etch_recipe import load_values, get_I_dt
 
 class PSiReflectanceSimulator:
+    lbd_min_nm = 400
+    lbd_max_nm = 850
     def __init__(self, stack):
         self.stack = stack
         self.lbd = self.R = self.T = None
@@ -14,12 +16,14 @@ class PSiReflectanceSimulator:
     def compute_refl_spectum(self, wavelengths=None):
         self.lbd = wavelengths
         if wavelengths is None:
-            self.lbd = np.linspace(450, 850, 100)*1e-9
+            self.lbd = np.linspace(self.lbd_min_nm, self.lbd_max_nm, 100)*1e-9
         n_wavelength = len(self.lbd)
         self.R = np.zeros(n_wavelength)
         self.T = np.zeros(n_wavelength)
+        c_arr = stack.get_c_array()
+        allocate_matrices(c_arr)
         for i_lbd, lbd in enumerate(self.lbd):
-            t_arr, n_arr, c_arr = self.stack.get_t_n_c_arrays(lbd)
+            t_arr, n_arr = self.stack.get_t_n_arrays(lbd)
             self.R[i_lbd], self.T[i_lbd] = tmm_get_R_T(lbd, t_arr, n_arr, c_arr)
         return self.R, self.T
         # TODO: adapted fct
@@ -32,13 +36,40 @@ class PSiReflectanceSimulator:
     def load_spectrum(self):
         self.lbd, self.R, self.T = load_prev_data()
 
+    def load_from_ocean_view(self, fname):
+        lbd_nm, R_large = np.loadtxt(fname, skiprows=14, unpack=True)
+        c1 = self.lbd_min_nm < lbd_nm
+        c2 = lbd_nm < self.lbd_max_nm
+        ind_to_keep = np.where(np.logical_and(c1, c2))
+        self.lbd = lbd_nm[ind_to_keep]*1e-9
+        self.R = R_large[ind_to_keep]/100.
+        self.T = np.zeros(len(self.R))
+
     def show_R_T(self, image_name=None):
         lbd_nm = self.lbd * 1e9
         plot_show_R_T(lbd_nm, self.R, self.T, image_name)
 
     def compare_R(self, loaded_simulator, im_name=None):
         lbd_nm = self.lbd * 1e9
-        plot_compare_R(lbd_nm, self.R, other.R, im_name)
+        plot_compare_R(lbd_nm, self.R, loaded_simulator.R, im_name)
+
+    def extract_Rp_FWHM(self):
+        Rp = self.R[0]
+        lbd_p = self.lbd[0]
+        iC = 0
+        for i in range(1, len(self.R)):
+            if self.R[i] > Rp:
+                Rp = self.R[i]
+                lbd_p = self.lbd[i]
+                iC = i
+        iL = iR = iC
+        while self.R[iL] > Rp/2.:
+            iL -= 1
+        while self.R[iR] > Rp/2.:
+            iR += 1
+        fwhm = self.lbd[iR] - self.lbd[iL]
+        return Rp, fwhm
+
 
 class PSiOpticalStack:
     def __init__(self, n_method, p_J_r_data_location="data-Clementine"):
@@ -97,28 +128,52 @@ class PSiOpticalStack:
         self.cells_p.append([porosity])
         self.cells_mult.append(1)
 
-    def add_Bragg_stack(self, t1, t2, p1, p2, N, rugosity=0, transition_layers=False):
+    def add_Bragg_stack(self, t1, t2, p1, p2, N, rugosity=0, transition_layer=None):
         # TODO: rugosity, intermediate layers
+        dt = 0.
+        if transition_layer is not None:
+            dt = transition_layer
+        p_transition = (p1+p2)/2.
         for _ in range(N):
-            self.layers_t.insert(self.append_index, t1)
+            self.layers_t.insert(self.append_index, t1-dt)
             self.layers_p.insert(self.append_index, p1)
             self.layers_c.insert(self.append_index, 'c')
             self.append_index += 1
-            self.layers_t.insert(self.append_index, t2)
+            if transition_layer is not None:
+                self.layers_t.insert(self.append_index, dt)
+                self.layers_p.insert(self.append_index, p_transition)
+                self.layers_c.insert(self.append_index, 'c')
+                self.append_index += 1
+            self.layers_t.insert(self.append_index, t2-dt)
             self.layers_p.insert(self.append_index, p2)
             self.layers_c.insert(self.append_index, 'c')
             self.append_index += 1
+            if transition_layer is not None:
+                self.layers_t.insert(self.append_index, dt)
+                self.layers_p.insert(self.append_index, p_transition)
+                self.layers_c.insert(self.append_index, 'c')
+                self.append_index += 1
+        if transition_layer is not None:
+            self.layers_t[self.append_index-4*N] += dt/2.
+            self.layers_t[self.append_index-2] += dt/2.
+            self.append_index -= 1
+            self.layers_t.pop(self.append_index)
+            self.layers_p.pop(self.append_index)
+            self.layers_c.pop(self.append_index)
         self.cells_t.append([t1, t2])
         self.cells_p.append([p1, p2])
         self.cells_mult.append(N)
 
-    def get_t_n_c_arrays(self, wavelength):
+    def get_t_n_arrays(self, wavelength):
         layers_n = []
         for p in self.layers_p:
             # TODO: improvement possible (repetitions)
             n = self.n_method.get_complex_n(wavelength, p)
             layers_n.append(n)
-        return self.layers_t, layers_n, self.layers_c
+        return self.layers_t, layers_n
+
+    def get_c_array(self):
+        return self.layers_c
 
     def produce_I_dt_data(self, sample_area=0.27):
         # TODO: write in file
@@ -152,15 +207,23 @@ if __name__ == '__main__':
     stack = PSiOpticalStack(m)
     stack.reset_stack()
     t1, t2, p1, p2, N = stack.optimal_Bragg_stack(633e-9)
-    print("Impose N=10")
-    stack.add_Bragg_stack(t1, t2, p1, p2, 10)
+    N = 10
+    print(f"Impose N={N}")
+    stack.add_Bragg_stack(t1, t2, p1, p2, N, transition_layer=10.e-9)
     # stack.add_Bragg_stack(0, 0, 0, 0, 40)
     t_supp = 80e-6  # 80 um support
-    p_supp = 0.5
+    p_supp = 0.7   # TODO: this is 0.5
     stack.add_thick_layer(t_supp, p_supp)
+
+    s_test = PSiReflectanceSimulator(None)
+    s_test.load_spectrum()
+    #s_test.load_from_ocean_view("temp/ad5_Relative__0__20.txt")
     simu = PSiReflectanceSimulator(stack)
-    simu.compute_refl_spectum()
-    simu.show_R_T("simu_R_50-70p_Oox.png")
-    stack.produce_I_dt_data()
+    simu.compute_refl_spectum(s_test.lbd)
+    #simu.save_spectrum()
+    simu.show_R_T("test_method_loc.png")
+    #Rp, d_lbd = simu.extract_Rp_FWHM()
+    simu.compare_R(s_test, "test_R_50-70p_Oox.png")
+    #stack.produce_I_dt_data()
 
 
--- reflectance.py
+++ reflectance.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tmm.tmm_core import inc_tmm
+from tmm.tmm_core import inc_tmm, power_entering_from_r
 from n_index import get_n_eff, target_lbd
 
 """
@@ -10,6 +10,124 @@ dh = 77     # thickness [nm]
 nl = 1.65   # other layer
 dl = 98
 """
+stacks = None
+inc_layers = None
+
+M_interface = None
+M_travel = None
+M_layer = None
+
+L_travel = None
+L_interface = None
+L_stack = None
+L_equ = None
+def allocate_matrices(c_arr):
+    global M_interface
+    M_interface = np.empty((len(c_arr), 2, 2))
+
+def load(matrix, a, b, c, d):
+    matrix[0,0] = a
+    matrix[0,1] = b
+    matrix[1,0] = c
+    matrix[1,1] = d
+
+def set_stacks()
+
+def coh_R_T(pol, n_arr, t_arr, lbd_0):
+    # (1  r) = M_ (t  0)
+    # Ef1 = M_11 Ef2 + M_12 Eb2
+    # Eb1 = M_21 Ef2 + M_22 Eb2
+    pol_fact = 1.
+    if pol == "p":
+        pol_fact = -1.
+    elif pol != "s":
+        raise ValueError(f"Polarization must be 's' or 'p', got {pol}")
+    N = len(t_arr)
+    assert (N >= 2) and (N == len(n_arr))  # at least 2 layers to get an interface
+    # between layer 0 and 1
+    n1 = n_arr[0]
+    n2 = n_arr[1]
+    r = pol_fact*(n1-n2)/(n1+n2)
+    t = 2*n1/(n1+n2)
+    M_interface_0 = np.matrix([[1, r], [r, 1]], dtype=complex) / t
+    M_layer = [M_interface_0]
+    for i in range(1, N-1):
+        # inside layer i
+        kz = 2 * np.pi * n_arr[i] / lbd_0
+        delta = t_arr[i]*kz  # phase shift inside layer i
+        M_travel_i = np.matrix([[np.exp(-1j*delta), 0], [0, np.exp(1j*delta)]])
+        # between layer i and i+1
+        n1 = n_arr[i]
+        n2 = n_arr[i+1]
+        r = pol_fact*(n1-n2)/(n1+n2)
+        t = 2*n1/(n1+n2)
+        M_interface_i = np.matrix([[1, r], [r, 1]]) / t
+        M_layer.append(np.dot(M_travel_i, M_interface_i))
+
+    M_equ = M_layer[0]
+    for M in M_layer[1:N-1]:
+        M_equ = np.dot(M_equ, M)
+    r = M_equ[1, 0]/M_equ[0, 0]
+    t = 1./M_equ[0, 0]
+    T_fact = n_arr[N-1].real/n_arr[0].real
+    R = r.real*r.real+r.imag*r.imag
+    T = (t.real*t.real+t.imag*t.imag)*T_fact
+    return R, T
+
+def nat_get_R_T_with_pol(pol, n_arr, t_arr, c_arr, theta=0, lbd_0=633e-9):
+    # @PRE: infinite non-absorbing incoherent first and last layers, theta=0.+0.j
+    if isinstance(n_arr, list):
+        n_arr = np.array(n_arr, dtype=complex)
+    if isinstance(t_arr, list):
+        t_arr = np.array(t_arr)
+    assert (len(n_arr) >= 2) and (len(n_arr) == len(t_arr) == len(c_arr))
+    stacks = []
+    stack_ind = [0]
+    # power going through a layer (not absorbed): from layer 1 to N-1
+    P_list = [1.]  # No loss before 2nd layer
+    for i in range(1, len(c_arr)):
+        stack_ind.append(i)
+        if c_arr[i] == 'i':
+            stacks.append(np.array(stack_ind))
+            if i != len(c_arr)-1:  # we don't compute losses and reflection in/after last layer
+                P = np.exp(-4*np.pi*n_arr[i].imag*t_arr[i]/lbd_0)
+                if P < 1e-30:  # Avoid div by 0 errors (opaque layer)
+                    P = 1e-30
+                P_list.append(P)
+            else:
+                P_list.append(1.)
+            stack_ind = [i]
+    # Calculating power, R, T (used for incoherent layers)
+    T_list = []  # power transmitted or reflected at each interface
+    R_list = []
+    T_inv_list = []
+    R_inv_list = []
+    for stack_ind in stacks:
+        R, T = coh_R_T(pol, n_arr[stack_ind], t_arr[stack_ind], lbd_0)
+        R_list.append(R)
+        T_list.append(T)
+    for stack_ind in stacks:
+        inv_stack_ind = stack_ind[::-1]
+        R, T = coh_R_T(pol, n_arr[inv_stack_ind], t_arr[inv_stack_ind], lbd_0)
+        R_inv_list.append(R)
+        T_inv_list.append(T)
+    R_inv_list = R_inv_list
+    T_inv_list = T_inv_list
+
+    # Computing matrices for power transmission
+    L_list = []
+    R_diff = T_list[0]*T_inv_list[0]  - R_list[0]*R_inv_list[0]
+    L_equ = np.matrix([[1, -R_inv_list[0]], [R_list[0], R_diff]]) / T_list[0]
+    L_list.append(L_equ)
+    for i in range(1, len(P_list)-1):
+        # Losses in layer incoherent_layers_ind[i] and reflection between this one and next incoherent layer
+        L_travel_i = np.matrix([[1./P_list[i], 0], [0, P_list[i]]])
+        R_diff = T_list[i]*T_inv_list[i] - R_list[i]*R_inv_list[i]
+        L_interface_i = np.matrix([[1, -R_inv_list[i]], [R_list[i], R_diff]]) / T_list[i]
+        L_i = np.dot(L_travel_i, L_interface_i)
+        L_equ = np.dot(L_equ, L_i)
+        L_list.append(L_i)
+    return {'R':L_equ[1, 0]/L_equ[0, 0], 'T':1./L_equ[0, 0]}
 
 def tmm_compute(lbd_arr, t_arr, r_arr, solv="air"):
     nfrequ = len(lbd_arr)
@@ -26,8 +144,8 @@ def tmm_compute(lbd_arr, t_arr, r_arr, solv="air"):
 
 def tmm_get_R_T(wavelength, t_arr, n_arr, c_arr):
     theta = 0.  # incident ray angle [rad]
-    s_data = inc_tmm('s', n_arr, t_arr, c_arr, theta, wavelength)
-    p_data = inc_tmm('p', n_arr, t_arr, c_arr, theta, wavelength)
+    s_data = nat_get_R_T_with_pol('s', n_arr, t_arr, c_arr, theta, wavelength)  # inc_tmm nat_get_R_T_with_pol
+    p_data = nat_get_R_T_with_pol('p', n_arr, t_arr, c_arr, theta, wavelength)
     # Keys in s/p_data dictionnary:
     # ['r', 't', 'R', 'T', 'power_entering', 'vw_list', 'kz_list', 'th_list',
     # 'pol', 'n_list', 'd_list', 'th_0', 'lam_vac']
@@ -37,15 +155,15 @@ def tmm_get_R_T(wavelength, t_arr, n_arr, c_arr):
 
 def save_data(lbd_arr, R_arr, T_arr, fname=None):
     if fname is None:
-        fname = "temp/reflectance_transmittance.txt"
-    np.savetxt(fname,
+        fname = "reflectance_transmittance.txt"
+    np.savetxt("temp/"+fname,
                np.transpose([lbd_arr, R_arr, T_arr]), header="lbd[m] R[-] T[-]")
 
 def load_prev_data(fname=None):
     if fname is None:
-        fname = "temp/reflectance_transmittance.txt"
+        fname = "reflectance_transmittance.txt"
     # Get 'lbd, R, T' from previous execution
-    return np.loadtxt(fname, skiprows=1, unpack=True)
+    return np.loadtxt("temp/"+fname, skiprows=1, unpack=True)
 
 def show_R_T_at_target(lbd, R, T, target_lbd):
     for i, l in enumerate(lbd):
@@ -54,10 +172,11 @@ def show_R_T_at_target(lbd, R, T, target_lbd):
             break
 
 def plot_show_R_T(lbd_nm, R, T, fname=None):
+    plt.figure()
     plt.plot(lbd_nm, R, label="R")
     plt.plot(lbd_nm, T, label="T")
 
-    plt.xlabel("lbd [nm]")
+    plt.xlabel("$\\lambda$ [nm]")
     plt.ylabel("R, T [-]")
     plt.legend()
     if fname is not None:
@@ -65,16 +184,17 @@ def plot_show_R_T(lbd_nm, R, T, fname=None):
     plt.show()
 
 def plot_compare_R(lbd_nm, R_ref, R_test, fname=None):
-    err = np.abs(R_ref - R_test)/R_ref * 100.
-    fig, ax = plt.subplots(1, 2)
+    err = np.abs(R_ref - R_test)
+    # default fig size: [6.4, 4.8]
+    fig, ax = plt.subplots(1, 2, sharey=False, figsize=(10., 4.8))
     ax[0].plot(lbd_nm, R_ref, label="Model")
     ax[0].plot(lbd_nm, R_test, label="Measurement")
     ax[1].plot(lbd_nm, err)
 
-    ax[0].set_xlabel("lbd [nm]")
+    ax[0].set_xlabel("$\\lambda$ [nm]")
     ax[0].set_ylabel("R [-]")
-    ax[1].set_xlabel("lbd [nm]")
-    ax[1].set_ylabel("Error [%]")
+    ax[1].set_xlabel("$\\lambda$ [nm]")
+    ax[1].set_ylabel("Absolute error [-]")
     ax[0].legend()
     if fname is not None:
         fig.savefig("results/" + fname)
